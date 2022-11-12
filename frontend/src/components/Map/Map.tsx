1f3cdaf95a3b16ea2066c1c5e9c6efe3dee77c19
JuanQP <quinterosp.juan@gmail.com>
2022-11-12 17:51:41 -0300

    Fix Leaflet icon images error

--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,18 +1,20 @@
 import { hasLatLong } from '@/helpers';
 import L from 'leaflet';
-import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import iconMarker from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { DomicilioMarker } from "./DomicilioMarker";
-// This is neccesary to make Leaflet markers work.
-// delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: iconRetina,
-    iconUrl: iconMarker,
-    shadowUrl: markerShadow,
+
+const DefaultIcon = L.icon({
+  iconUrl: iconMarker,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
 });
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface Props {
   initialPosition: [number, number];
